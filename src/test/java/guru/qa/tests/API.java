4c2505f098de755127307f35c218a716f2c0e7dd
Ruslan Ibraev <Ruslan.Ibraev@mediascope.net>
2024-10-01 17:54:19 +0300

    added Json

--- src/test/java/guru/qa/tests/API.java
+++ src/test/java/guru/qa/tests/API.java
@@ -1,16 +1,22 @@
 package guru.qa.tests;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class API {
     @Test
     void loginTest() {
         String body = "{ \"email\": \"eve.holt@reqres.in\", " +
                 "\"password\": \"cityslicka\" }";
+
 
         given()
                 .log().uri()
@@ -43,4 +49,41 @@ public class API {
                 .statusCode(400)
                 .body("error", is("Missing password"));
     }
+
+    @Test
+    void loginTestNext() {
+        String body = "{ \"email\": \"eve.holt@reqres.in\", " +
+                "\"password\": \"cityslicka\" }";
+
+       // JSONObject jsonObj = new JSONObject(jsonString.toString());
+        System.out.println();
+        Response response = RestAssured.given()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .extract().response();
+
+        String readableTest = response.asPrettyString();
+        System.out.println(readableTest);
+
+
+       // String json = "{ \"name\": \"Baeldung\", \"java\": true }";
+
+        JsonObject jsonObject = JsonParser.parseString(readableTest)
+                .getAsJsonObject();
+
+        assertTrue(jsonObject.isJsonObject());
+        System.out.println(jsonObject);
+        String woo = jsonObject.get("token").getAsString();
+        System.out.println(woo);
+
+
+    }
+
+
 }

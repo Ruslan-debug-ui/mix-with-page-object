55825c200c642d08deec6eb96a729a4c35ff799c
Ruslan Ibraev <Ruslan.Ibraev@mediascope.net>
2024-10-31 01:22:14 +0300

    google test some id/name changes

--- src/test/java/guru/qa/tests/GoogleTest.java
+++ src/test/java/guru/qa/tests/GoogleTest.java
@@ -1,7 +1,6 @@
 package guru.qa.tests;
 
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -11,11 +10,13 @@ public class GoogleTest {
     public void user_can_search_everything_in_google() {
         open("http://google.com/ncr");
       //  $(By.id("APjFqb")).val("selenide").pressEnter();
-        $(By.name("q")).val("selenide").pressEnter();
+    //    $(By.name("q")).val("selenide").pressEnter();
+    //    $("[name=q]").val("selenide").pressEnter();
+        $("[id=APjFqb]").val("selenide").pressEnter();
 
 //////        $$("#ires .g").shouldHave(size(10));
 ////
-      //  $(withText("Selenide vs Selenium")).click();
+      //  $("[data-hveid='CAgQAA']").shouldHave(text("Selenide vs Selenium"));
 //                text("Selenide: concise UI tests in Java"),
 //                text("selenide.org"));
     }
